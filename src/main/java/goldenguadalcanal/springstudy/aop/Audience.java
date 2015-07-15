ea9b0ff253c122be0b7c7a89e27db6a7d0c26fcb
goldenguadalcanal <golden_guadalcanal@163.com>
2015-07-15 08:59:46 +0800

    upload

--- src/main/java/goldenguadalcanal/springstudy/aop/Audience.java
+++ src/main/java/goldenguadalcanal/springstudy/aop/Audience.java
@@ -10,7 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Aspect
-public class Audience {
+public class Audience {   
 	
 	private Logger log = LoggerFactory.getLogger(getClass());
 	
